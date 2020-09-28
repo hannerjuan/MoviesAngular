08781e355c6bef46edc3fca3ccd2b41bfba14e5e
Hanner Obando <hannerobando@outlook.com>
2020-09-28 15:24:31 -0500

    All components of master-page is added

---
+++ MoviesNG/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DefaultComponent } from './public/home/default/default.component';
+import { NavbarComponent } from './public/master-page/navbar/navbar.component';
+import { FooterComponent } from './public/master-page/footer/footer.component';
+import { HomeComponent } from './public/master-page/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DefaultComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
