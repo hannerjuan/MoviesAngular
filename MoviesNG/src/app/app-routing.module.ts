994735e37aa8f5491ca3331994b2a18d92435593
Hanner Obando <hannerobando@outlook.com>
2020-09-29 00:36:04 -0500

    Routes's management is added

--- MoviesNG/src/app/app-routing.module.ts
+++ MoviesNG/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DefaultComponent} from './public/home/default/default.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    path: 'home',
+    component: DefaultComponent
+  },
+  {
+    path: 'movies',
+    loadChildren: () => import('./modules/parameters/parameters-routing.module').then(m => m.ParametersRoutingModule)
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MoviesNG/src/app/modules/parameters/parameters-routing.module.ts
+++ MoviesNG/src/app/modules/parameters/parameters-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MovieCreationComponent} from './movie/movie-creation/movie-creation.component'
+import {MovieListComponent} from './movie/movie-list/movie-list.component'
+import {MovieEditionComponent} from './movie/movie-edition/movie-edition.component'
+import {MovieRemoveComponent} from './movie/movie-remove/movie-remove.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'movie-creation',
+    component: MovieCreationComponent
+  },
+  {
+    path: 'movie-list',
+    component: MovieListComponent
+  },
+  {
+    path: 'movie-edition',
+    component: MovieEditionComponent
+  },
+  {
+    path: 'movie-remove',
+    component: MovieRemoveComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
