a0508c37c5b53b8de2d257be356f965c025bc39d
Hanner Obando <hannerobando@outlook.com>
2020-09-29 12:53:35 -0500

    The navbar, footer, and routing are changed.

---
+++ MoviesNG/src/assets/scripts/materialize-init.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elemensDropdown = document.querySelectorAll(".dropdown-trigger");
+    var instances = M.Dropdown.init(elemensDropdown);
+}); 
